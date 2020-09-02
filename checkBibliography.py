9b866ee1843ef7a6a786016201ce8143616f6fb8
redrover9 <grace.thompson97@gmail.com>
2020-09-02 05:09:53 -0700

    Update checkBibliography.py

--- checkBibliography.py
+++ checkBibliography.py
@@ -1,8 +1,11 @@
 import re
 import unittest
 import textract
+import os
+import shutil
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException as WDE
 
 essay = textract.process("/home/grace/Downloads/essay.pdf")
 essayDecoded = essay.decode()
@@ -14,33 +17,45 @@ httpOccurrencesLen = len(httpOccurrences)
 linksUnformatted = re.findall(r'(https?://[^\s]+[\.]?)', essayDecoded) #If the student has a period at the end of their links, remove the question mark.
 links = [x[:-1] for x in linksUnformatted]
 linksLen = len(links)
-refRegex = r'"(.*?)"'
-refs = re.findall(refRegex, essayDecoded)
-print(essayDecoded)
 
-citations = {}
+citations = set()
 i = 0
 while i < linksLen: 
-    citations["link{0}".format(i)] = links[i]
+    citations.add(links[i])
     i += 1
 
-"""
-quote = "Healthcare"
+citations = list(citations)
+quote = input("Enter a quote to search the student's citations for: ")
 
 class PythonOrgSearch(unittest.TestCase):
-
     def setUp(self):
-        self.driver = webdriver.Firefox()
-
+        profile = webdriver.FirefoxProfile()
+        profile.set_preference("browser.download.folderList",2)
+        profile.set_preference("browser.helperApps.alwaysAsk.force", False)
+        profile.set_preference("browser.download.manager.showWhenStarting",False)
+        profile.set_preference("browser.download.dir", "/home/grace/Downloads")
+        profile.set_preference("plugin.disable_full_page_plugin_for_types", "application/pdf")
+        profile.set_preference("pdfjs.disabled", True)
+        profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/pdf")  
+        self.driver = webdriver.Firefox(firefox_profile=profile)
     def test_search_in_python_org(self):
         driver = self.driver
-        for key in citations.keys():
-            driver.get(citations[key])
-            assert quote in driver.page_source
+        for citation in citations:
+            try: 
+                driver.get(citation)
+                if 'pdf' in citation:
+                    pdfText = textract.process('0029682-003-XIE.pdfpage27.pdf')
+                    pdfTextDecoded = pdfText.decode()
+                    assert quote in pdfTextDecoded
+                    print("The quote was found in a PDF file downloaded from", citation)
+                else:
+                    assert quote in driver.page_source
+                print("The quote was found on the web page", citation)
+            except WDE:
+                print("The web page at", citation, "did not load correctly. It may require logging in.")
+            except AssertionError:
+                print("The citation was not found on page", citation)
     def tearDown(self):
         self.driver.close()
-
 if __name__ == "__main__":
     unittest.main()
-
-"""
